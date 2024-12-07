5ab14b479cee985d23aa57a6c7fa40e403db2bb7
DmitryTaushev <123morfix@gmail.com>
2024-12-07 13:27:22 +0500

    Выполнили таск_1

--- task_1.py
+++ task_1.py
@@ -17,3 +17,37 @@
 # студента и его оценку, результат вызова метода:
 # give_a_consultation в качестве аргумента принимает, класс
 # в котором учатся ученики, результат вызова метода:
+
+class Teacher:
+    def __init__(self,name,education,xp):
+        self.__name = name
+        self.__education = education
+        self.__xp = xp
+
+    def get_name(self):
+        return self.__name
+
+    def get_education(self):
+        return self.__education
+
+    def set_xp(self,value):
+        self.__xp = value
+
+    def get_teacher_data(self):
+        return print(f"{self.__name}, образование {self.__education}, опыт работы {self.__xp} года")
+
+    def add_mark(self,student_name,student_mark):
+        return print(f'{self.__name} поставил оценку {student_mark} студенту {student_name}')
+
+    def remove_mark(self,student_name,student_mark):
+        return print(f'{self.__name} удалил оценку {student_mark} студенту {student_name}')
+
+    def give_a_consultation(self,student_class):
+        return print(f'{self.__name} провел консультацию в классе {student_class}')
+
+teacher = Teacher("Иван Петров","БГПУ","4")
+
+teacher.get_teacher_data()
+teacher.add_mark('Петр Борисов',"5")
+teacher.remove_mark('Даниил Васильев',"3")
+teacher.give_a_consultation("9Б")
